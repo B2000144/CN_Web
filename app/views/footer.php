e9a932aef73d360bb3817c92b623c16746fa940c
B2000144 <125292845+B2000144@users.noreply.github.com>
2023-11-18 00:03:45 +0700

    first commit

---
+++ app/views/footer.php
@@ -0,0 +1,86 @@
+  <footer>
+      <div class="footer_top_bootom">
+          <div class="bg_in">
+              <div class="footer footer_bottom">
+                  <div class="footer-col">
+                      <h4>INDEPENDENT OPTICAL</h4>
+
+                  </div>
+                  <div class="footer-col">
+                      <h4>KHÁM PHÁ</h4>
+                      <div class="footer-col-content">
+                          <ul>
+                              <li><a href="#">Thiết kế đơn giản</a></li>
+                              <li><a href="#">Hệ thống cửa hàng</a></li>
+                          </ul>
+                      </div>
+                  </div>
+                  <div class="footer-col">
+                      <h4>DỊCH VỤ TẠI CỬA HÀNG</h4>
+                      <div class="footer-col-content">
+                          <ul>
+                              <li><a href="#">Tư vấn & chăm sóc</a></li>
+                              <li><a href="#">24/24</a></li>
+                              <li><a href="#">Giao hàng tận nơi</a></li>
+                              <li><a href="#">Dịch vụ trực tuyến</a></li>
+                              <li><a href="#">Cam kết chính hãng</a></li>
+                          </ul>
+                          <div class="clear"></div>
+                      </div>
+                  </div>
+                  <div class="footer-col">
+                      <h4>LIÊN HỆ</h4>
+                      <div class="footer-col-content">
+                          <ul class="icon_list">
+                              <li>
+                                  <a href=".">
+                                      <i class="fa fa-mobile" aria-hidden="true"></i>
+                                      <p class="text_li">
+                                          <span class="phone_footer">Hotline: 0123 568 </span>
+                                          <span class="text_small">(Từ 8h đến 20h tất cả các ngày)</span>
+                                      </p>
+                                  </a>
+                              </li>
+                              <div class="clear"></div>
+                              <li><a><i class="fa fa-envelope" aria-hidden="true"></i>
+                                      trib2000144@student.ctu.edu.vn</a></li>
+                              <li><a><i class="fa fa-mouse-pointer" aria-hidden="true"></i> http://localhost/</a>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+              <div class="clear"></div>
+          </div>
+          <div class="clear"></div>
+      </div>
+      <div class="clear"></div>
+      <div class="clear"></div>
+  </footer>
+  <link async rel="stylesheet" href="<?php echo BASE_URL ?>/public/css/cssfooter.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script defer type="text/javascript" src="<?php echo BASE_URL ?>/public/js/bootstrap.js"></script>
+  <script defer type="text/javascript" src="<?php echo BASE_URL ?>/public/js/sweet-alert.js"></script>
+  <script defer type="text/javascript" src="<?php echo BASE_URL ?>/public/js/jquery.flexslider-min.js"></script>
+  <script defer type="text/javascript"
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script defer src="<?php echo BASE_URL ?>/public/js/jquery.lazyload.min.js" type="text/javascript"></script>
+  <script defer type="text/javascript" src="<?php echo BASE_URL ?>/public/js/script_ex.js"></script>
+  <script defer type="text/javascript" src="<?php echo BASE_URL ?>/public/js/script_menu.js"></script>
+  <!-- <link rel="stylesheet" type="text/css" href="template/Default/js/sweet-alert.css"/> -->
+  <div id="fb-root"></div>
+  <script async defer crossorigin="anonymous"
+      src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v9.0&appId=297476534767349&autoLogAppEvents=1"
+      nonce="6Np37FtJ"></script>
+
+
+
+  <div style="height: 1840px;position: fixed;width: 100%;top: 0px;left: 0px;right: 0px;bottom: 0px;z-index: 1001;background: #000 none repeat scroll 0% 0%;opacity: 0.3;display: none;text-align:center"
+      id="khungnen"></div>
+  <div style="display: none;left: 70%;margin-left: -309px;z-index: 1002;position: fixed;top: 40%;margin-top: 0px;"
+      id="loadding">
+      <img src="image/loader.gif" />
+  </div>
+  </body>
+
+  </html>
