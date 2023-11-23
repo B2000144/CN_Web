f4efac3505af07a091c6f0fedab6d4a4a3bbe320
B2000144 <125292845+B2000144@users.noreply.github.com>
2023-11-24 01:44:31 +0700

    hoan thanh

--- app/views/cart.php
+++ app/views/cart.php
@@ -49,71 +49,82 @@
                               <?php
                                 if (isset($_SESSION['shopping_cart'])) {
                                 ?>
-                                  <form action="<?php echo BASE_URL ?>/giohang/updategiohang" method="POST">
-                                      <?php
+                              <form action="<?php echo BASE_URL ?>/giohang/updategiohang" method="POST">
+                                  <?php
                                         $total = 0;
                                         foreach ($_SESSION['shopping_cart'] as $key => $value) {
                                             $subtotal = $value['product_quantity'] * $value['product_price'];
                                             $total += $subtotal;
                                         ?>
-                                          <tr class="tr">
-                                              <td data-th="Hình ảnh">
-                                                  <div class="col_table_image col_table_hidden-xs"><img src="<?php echo BASE_URL ?>/public/uploads/product/<?php echo $value['product_image'] ?>" alt="<?php echo $value['product_title'] ?>" class="img-responsive" />
-                                                  </div>
-                                              </td>
-                                              <td data-th="Sản phẩm">
-                                                  <div class="col_table_name">
-                                                      <h4 class="nomargin"><?php echo $value['product_title'] ?></h4>
-                                                  </div>
-                                              </td>
-
-                                              <td data-th="Giá"><span class="color_red font_money"><?php echo number_format($value['product_price'], 0, ',', '.') . 'đ' ?></span>
-                                              </td>
-
-                                              <td data-th="Số lượng">
-                                                  <div class="clear margintop5">
-                                                      <div class="floatleft">
-
-                                                          <input type="number" min="1" class="inputsoluong" name="qty[<?php echo $value['product_id'] ?>]" value="<?php echo $value['product_quantity'] ?>">
-                                                      </div>
-
-                                                  </div>
-                                                  <div class="clear"></div>
-                                              </td>
-
-                                              <td data-th="Thành tiền" class="text_center"><span class="color_red font_money"><?php echo number_format($subtotal, 0, ',', '.') . 'đ' ?></span>
-                                              </td>
-
-                                              <td class="actions aligncenter">
-
-                                                  <button type="submit" style="box-shadow: none;" value="<?php echo $value['product_id'] ?>" name="delete_cart" class="btn btn-sm btn-warning">Xóa</button>
-
-                                                  <button type="submit" style="box-shadow: none;" value="<?php echo $value['product_id'] ?>" name="update_cart" class="btn btn-sm btn-primary">Cập nhật</button>
-                                              </td>
-
-                                          </tr>
-                                      <?php
-                                        }
-                                        ?>
-                                  </form>
-                                  <tr>
-                                      <td colspan="7" class="textright_text">
-                                          <div class="sum_price_all">
-                                              <span class="text_price">Tổng tiền thành toán</span>:
-                                              <span class="text_price color_red"><?php echo number_format($total, 0, ',', '.') . 'đ' ?></span>
+                                  <tr class="tr">
+                                      <td data-th="Hình ảnh">
+                                          <div class="col_table_image col_table_hidden-xs"><img
+                                                  src="<?php echo BASE_URL ?>/public/uploads/product/<?php echo $value['product_image'] ?>"
+                                                  alt="<?php echo $value['product_title'] ?>" class="img-responsive" />
                                           </div>
                                       </td>
+                                      <td data-th="Sản phẩm">
+                                          <div class="col_table_name">
+                                              <h4 class="nomargin"><?php echo $value['product_title'] ?></h4>
+                                          </div>
+                                      </td>
+
+                                      <td data-th="Giá"><span
+                                              class="color_red font_money"><?php echo number_format($value['product_price'], 0, ',', '.') . 'đ' ?></span>
+                                      </td>
+
+                                      <td data-th="Số lượng">
+                                          <div class="clear margintop5">
+                                              <div class="floatleft">
+
+                                                  <input type="number" min="1" class="inputsoluong"
+                                                      name="qty[<?php echo $value['product_id'] ?>]"
+                                                      value="<?php echo $value['product_quantity'] ?>">
+                                              </div>
+
+                                          </div>
+                                          <div class="clear"></div>
+                                      </td>
+
+                                      <td data-th="Thành tiền" class="text_center"><span
+                                              class="color_red font_money"><?php echo number_format($subtotal, 0, ',', '.') . 'đ' ?></span>
+                                      </td>
+
+                                      <td class="actions aligncenter">
+
+                                          <button type="submit" style="box-shadow: none;"
+                                              value="<?php echo $value['product_id'] ?>" name="delete_cart"
+                                              class="btn btn-sm btn-warning">Xóa</button>
+
+                                          <button type="submit" style="box-shadow: none;"
+                                              value="<?php echo $value['product_id'] ?>" name="update_cart"
+                                              class="btn btn-sm btn-primary">Cập nhật</button>
+                                      </td>
+
                                   </tr>
+                                  <?php
+                                        }
+                                        ?>
+                              </form>
+                              <tr>
+                                  <td colspan="7" class="textright_text">
+                                      <div class="sum_price_all">
+                                          <span class="text_price">Tổng tiền thành toán</span>:
+                                          <span
+                                              class="text_price color_red"><?php echo number_format($total, 0, ',', '.') . 'đ' ?></span>
+                                      </div>
+                                  </td>
+                              </tr>
                               <?php
                                 } else {
                                 ?>
-                                  <tr>
-                                      <td colspan="7">
-                                          <div class="sum_price_all">
-                                              <span class="text_price">Giỏ hàng trống</span>
-                                          </div>
-                                      </td>
-                                  </tr>
+                              <tr>
+                                  <td colspan="7">
+                                      <div class="sum_price_all">
+                                          <span class="text_price">Giỏ hàng trống</span>
+                                      </div>
+                                  </td>
+                              </tr>
                               <?php
                                 }
                                 ?>
@@ -121,7 +132,8 @@
                           <tfoot>
                               <tr class="tr_last">
                                   <td colspan="7">
-                                      <a href="<?php echo BASE_URL ?>" class="btn_df btn_table floatleft"><i class="fa fa-long-arrow-left"></i> Tiếp tục mua hàng</a>
+                                      <a href="<?php echo BASE_URL ?>" class="btn_df btn_table floatleft"><i
+                                              class="fa fa-long-arrow-left"></i> Tiếp tục mua hàng</a>
                                       <div class="clear"></div>
                                   </td>
                               </tr>
@@ -150,7 +162,8 @@
                       <div class="contact_right">
                           <div class="form_contact_in">
                               <div class="box_contact">
-                                  <form name="FormDatHang" method="POST" autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]), BASE_URL ?>/giohang/dathang">
+                                  <form name="FormDatHang" method="POST" autocomplete="off"
+                                      action="<?php echo BASE_URL ?>/giohang/dathang">
                                       <div class="content-box_contact">
                                           <div class="row">
                                               <div class="input">
@@ -163,7 +176,8 @@
                                           <div class="row">
                                               <div class="input">
                                                   <label>Số điện thoại: <span style="color:red;">*</span></label>
-                                                  <input type="text" name="sodienthoai" required onkeydown="return checkIt(event)" class="clsip">
+                                                  <input type="text" name="sodienthoai" required
+                                                      onkeydown="return checkIt(event)" class="clsip">
                                               </div>
                                               <div class="clear"></div>
                                           </div>
@@ -179,7 +193,8 @@
                                           <div class="row">
                                               <div class="input">
                                                   <label>Email: <span style="color:red;">*</span></label>
-                                                  <input type="text" name="email" onchange="return KiemTraEmail(this);" required class="clsip">
+                                                  <input type="text" name="email" onchange="return KiemTraEmail(this);"
+                                                      required class="clsip">
                                               </div>
                                               <div class="clear"></div>
                                           </div>
@@ -194,7 +209,8 @@
                                           <!---row---->
                                           <div class="row btnclass">
                                               <div class="input ipmaxn ">
-                                                  <input type="submit" class="btn-gui" name="frmSubmit" id="frmSubmit" value="Gửi đơn hàng">
+                                                  <input type="submit" class="btn-gui" name="frmSubmit" id="frmSubmit"
+                                                      value="Gửi đơn hàng">
                                                   <input type="reset" class="btn-gui" value="Nhập lại">
                                               </div>
                                               <div class="clear"></div>
--- app/views/list_product.php
+++ app/views/list_product.php
@@ -3,7 +3,7 @@
         <div class="breadcrumbs">
             <ol itemscope itemtype="http://schema.org/BreadcrumbList">
                 <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                    <a itemprop="item" href=".">
+                    <a itemprop="item" href="<?php echo BASE_URL ?>">
                         <span itemprop="name">Trang chủ</span></a>
                     <meta itemprop="position" content="1" />
                 </li>
